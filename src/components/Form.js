db4e3ed02232fe828e3bead8e617d42f2681f118
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-18 18:33:03 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Form extends React.Component {
+export default class Form extends React.Component {
 
   // handleFirstNameChange = event => {
   //   this.setState({
